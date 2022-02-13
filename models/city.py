eff502340bc9e0a915a35fc9bc27dd6c543a0ae0
bakaryc50 <bakaryc50@gmail.com>
2022-02-13 10:42:30 -0800

    Bakary commit: fixing all test cases

--- models/city.py
+++ models/city.py
@@ -10,11 +10,12 @@ from sqlalchemy.orm import relationship
 
 class City(BaseModel, Base):
     """Representation of city """
-    if models.storage_t == "db":
+    if getenv('HBNB_TYPE_STORAGE') == 'db':
         __tablename__ = 'cities'
         state_id = Column(String(60), ForeignKey('states.id'), nullable=False)
         name = Column(String(128), nullable=False)
-        places = relationship("Place", backref="cities")
+        places = relationship("Place",
+                              backref="cities", cascade="all, delete-orphan")
     else:
         state_id = ""
         name = ""
